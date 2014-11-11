e07d86983bed172a47292b8df0ed99748fba210f
niechang <niechang@foxmail.com>
2014-11-11 20:40:52 +0800

    support next and previous button

--- js/blog.js
+++ js/blog.js
@@ -6,6 +6,7 @@
 	var ref = new Firebase("https://blistering-inferno-5110.firebaseio.com/"); 
 
 	var CACHE_TAGS;
+	var count = 0, current = 0;
 
 	function date2String(date) {
 		return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + ((date.getDate() > 9) ? date.getDate() : "0" + date.getDate())
@@ -19,21 +20,28 @@
                 console.log(err, 'error');
                 cb(err);
             } else if (user) {
-            	//uid = user.uid;
-               // console.log('logged in with id', uid);
                 cb(user);
             }
 		});
 	}
 
-	BLOG.Util.add = function(title,tags,text) {
-		var blogRef = blogsRef.push({
+	BLOG.Util.validateAuth = function (authData) {
+		return authData && authData.uid === "github:2278081";
+	}
+
+	BLOG.Util.getAuth = function (cb) {
+		var authData = ref.getAuth();
+		cb(authData);
+	}
+
+	BLOG.Util.add = function(title,tags,text,cb) {
+		var blogKey = count;
+		blogsRef.child(blogKey).set({
 			title: title,
 			tags: tags,
 			createTime: date2String(new Date())
 		});
-		var blogKey = blogRef.key();
-		textsRef.child(blogKey).set({ //saving text content
+		textsRef.child(blogKey).set({ //saving blog text content
 			text: text
 		});
 		var tagRefs = [];
@@ -50,6 +58,7 @@
 				tagRef.update(val);
 			});
 		});	
+		count++;
 	}
 
 	BLOG.Util.loadTags = function (cb) {
@@ -59,10 +68,53 @@
 		});
 	}
 
-	BLOG.Util.getLatestOne = function (cb) {
+	BLOG.Util.countPosts = function (cb) {
+		blogsRef.once("value", function(snap) {
+			val = snap.val();
+			if(!val) { 
+				count = 0;
+			}
+			else {
+				count = Object.keys(snap.val()).length;
+			}
+			cb && cb();
+		});
+	}
+
+	BLOG.Util.next = function (cb) {
+		if(current < count - 1) {
+			current++;
+			loadBlog(current,cb);
+		} else {
+			cb();
+		}
+	}
+
+	BLOG.Util.previous = function (cb) {
+		if(current > 0) {
+			current--;
+			loadBlog(current,cb);
+		} else {
+			cb();
+		}
+	}
+
+	BLOG.Util.hasNext = function () {
+		return current >= 0 && current < count - 1; 
+	}
+
+	BLOG.Util.hasPrevious = function () {
+		return current > 0 && count > 0;
+	}
+
+	function loadBlog(key,cb) {
+		if(current < 0) {
+			cb();
+			return;
+		}
 		var blog, tagNames = [];
-		blogsRef.limitToLast(1).on("child_added", function(snapshot) {
-  			blog = snapshot.val();
+		blogsRef.child(key).once("value",function(snapshot) {
+			blog = snapshot.val();
   			if (CACHE_TAGS) {
   				$.each(blog.tags,function(n,tag) {
   					tagNames.push(CACHE_TAGS[tag].name);
@@ -75,4 +127,19 @@
   			});
 		});
 	}
+
+	/** 
+	  * Get the latest one post in blogs
+	  * @param {Function} cb
+	  */
+	BLOG.Util.getLatestOne = function (cb) {
+		var blog, tagNames = [];
+		if (count === 0) {
+			BLOG.Util.countPosts(function () {
+				current = count - 1;
+				loadBlog(current, cb);
+			});
+		}
+	}
+	
 }());
